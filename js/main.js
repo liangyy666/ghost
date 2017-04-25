271df81162f52e3d5288a89483efe8eda9d4568a
liangyy666 <249266740@qq.com>
2017-04-25 16:54:08 +0800

    add
    new add

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function main(){
+	// hahah
+	// hahha
+	var name = "hello";
+	alert("hello, " + name);
+	alert(typeof(name));
+	alert(Number.MIN_VALUE);
+	// typeof是一个操作符而不是函数，常基本数据类型
+	// undefined 
+	// boolean
+	// string
+	// number
+	// object
+	// function
+	// !
+	// &&
+	// ||
+	// instanceof
+}
+
+main();
